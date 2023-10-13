5d3d37fc4e8d3b5282713262f8e12f1cf8452e70
gcarrasco <german.cb95@gmail.com>
2023-10-13 17:00:44 +0200

    Update clase Club

--- GestionEquipos/Club.cs
+++ GestionEquipos/Club.cs
@@ -8,12 +8,26 @@ namespace GestionEquipos
 {
     class Club
     {
-        List<Equipo> equipos;
+        private List<Equipo> equipos;
+        private string nombre;
+        private int anoFundacion;
+        private string nombreEstadio;
 
-       public Club()
-       {
+        public List<Equipo> Equipos { get { return equipos; } set { value = equipos; } }
+        public string Nombre { get { return nombre; } set { value = nombre; } }
+        public int AnoFundacion { get { return anoFundacion; } set { value = anoFundacion; } }
+        public string NombreEstadio { get { return nombreEstadio; } set { value = nombreEstadio; } }
+        public Club()
+        {
+            equipos = new List<Equipo>();
+        }
+        
+        public Club(string nombre, int anoFundacion, string nombreEstadio)
+        {
+            this.nombre = nombre;
+            this.anoFundacion = anoFundacion;
+            this.nombreEstadio = nombreEstadio;
+        }
 
-           equipos = new List<Equipo>();
-       }
     }
 }
