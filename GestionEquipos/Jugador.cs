f28ab1e3d6231649daef2dd90ad7e6b1a8708787
gcarrasco <german.cb95@gmail.com>
2023-10-13 16:35:48 +0200

    Añadida clase Jugador

---
+++ GestionEquipos/Jugador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEquipos
+{
+    internal class Jugador
+    {
+        private string nombre;
+        private string posicion;
+        private int dorsal;
+        private Club club;
+        private Equipo equipo;
+        
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Posicion { get { return posicion; } set { posicion = value; } }
+        public int Dorsal { get { return dorsal; } set { Dorsal = value; } }
+
+        public Jugador() { }
+        public Jugador(string nombre, string posicion, int dorsal, Club club, Equipo equipo)
+        {
+            Nombre = nombre;
+            Posicion = posicion;
+            Dorsal = dorsal;
+            Club = club;
+            Equipo = equipo;
+        }
+    }
+}
--- GestionEquipos/Program.cs
+++ GestionEquipos/Program.cs
@@ -10,6 +10,7 @@ namespace GestionEquipos
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
